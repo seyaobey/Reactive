8b385d658b15e6337817e67b57dff27787f5e272
Seya Obey <seyaobey@gmail.com>
2016-08-03 11:17:47 +0300

    rename project name

--- Reactive.v1/src/views/profiles/profiles_add_wizard.tsx
+++ Reactive.v1/src/views/profiles/profiles_add_wizard.tsx
@@ -22,11 +22,112 @@ export class ProfileAddWizard extends core.base.BaseView {
         var html = 
             <pn.BasePanel className="animated fadeInRight" style={{ minHeight: 350 }}>
                 <h2><i className="fa fa-plus-circle"></i> New profile</h2>
+
                 <hr />
+
+                {this.build_wizard()}
+
             </pn.BasePanel>
 
 
         return html;
+
+    }
+
+
+    componentDidMount() {
+
+        this.init_wizard();
+
+    }
+
+
+
+    build_wizard() {
+
+        var html =
+
+            <div id="new-art-wiz" className="row">
+
+                <h3>Basic info</h3>
+                <section>
+                    <h2>Profile basic information</h2>
+                </section>
+                <h3>Occupations</h3>
+                <section>
+                    <h2>Professional occupational selection</h2>
+                </section>
+                <h3>Profiles/Skills</h3>
+                <section>
+                    <h2>Professional profiles / skills selection</h2>
+                </section>
+                <h3>Skills</h3>
+                <section>
+                    <h2>Complete</h2>
+                </section>
+            </div>
+
+        return html;
+    }
+
+
+
+    init_wizard() {
+
+        var that = this;
+
+        this.root.find('#new-art-wiz')['steps']({
+            headerTag: "h3",
+            bodyTag: "section",
+            transitionEffect: "fade",
+            autoFocus: true,
+
+            onInit: (event, currentIndex) => {
+
+                //this.active_page = 0;
+
+                //this.get_active_page().activate_page();
+
+            },
+
+            onStepChanged: (event, current, prior) => {
+
+                //that.active_page = current;
+
+                //this.get_active_page().activate_page();
+            },
+
+            onStepChanging: (event, current, next) => {
+
+                var ok = true;
+
+                var forward = next > current;
+
+                //if (forward) {
+
+                //    if (current === 0) {
+
+                //        if (!this['searchindex'] || !this['keywords']) {
+                //            ok = false;
+                //        }
+                //    }
+
+                //    if (current === 1) {
+
+                //        var list = (this.refs['select-page'] as AmazonArticlesSelection_Page).selection_list;
+
+                //        if (!list || list.length === 0) {
+                //            ok = false;
+                //        }
+                //    }
+
+                //}
+
+
+
+                return ok;
+            }
+        });
 
     }
 
